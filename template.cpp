e3ea2fd1f9f7d5c40e3e4ce5378fce7482d2c786
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-04-17 22:19:01 +0300

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -6,4 +6,6 @@ using namespace std;
 int main()
 {
     cout << "daca citesti asta si te cheama neemia esti gay" << endl;
+    cout << "scuze raul a trebuit sa sterg ca nu mai imi descarca ce se intampla in cloud" << endl;
+    cout << "feel free to write it back" << endl;
 }
