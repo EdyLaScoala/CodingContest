97818eb8a95bd5e72e1d62fc0a4cd20a65c7b0ac
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-04-18 18:52:36 +0000

    e blestemat. nu merge nici macar atat.

--- Exercises/Bakery/Bakery.cpp
+++ Exercises/Bakery/Bakery.cpp
@@ -5,7 +5,9 @@ using namespace std;
 
 int main()
 {
-    cout << "daca citesti asta si te cheama neemia esti gay" << endl;
-    cout << "scuze raul a trebuit sa sterg ca nu mai imi descarca ce se intampla in cloud" << endl;
-    cout << "feel free to write it back" << endl;
+   ifstream fin("date.in");
+   int n;
+   fin >> n;
+   cout << n << endl;
+   return 0;
 }
