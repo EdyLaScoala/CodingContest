fa78f810a54a275fa51ec111a08d5f30918e9164
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-04-18 17:34:26 +0000

    Bakery

---
+++ Exercises/Bakery/Bakery.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    cout << "daca citesti asta si te cheama neemia esti gay" << endl;
+    cout << "scuze raul a trebuit sa sterg ca nu mai imi descarca ce se intampla in cloud" << endl;
+    cout << "feel free to write it back" << endl;
+}
