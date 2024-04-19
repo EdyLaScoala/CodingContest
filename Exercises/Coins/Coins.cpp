3f0f80ff3bae545c98b883d51bc459148066aae2
EdyLaScoala <145386331+EdyLaScoala@users.noreply.github.com>
2024-04-19 09:11:01 +0000

    Coins

--- Exercises/Coins/Coins.cpp
+++ Exercises/Coins/Coins.cpp
@@ -7,21 +7,49 @@ int main()
 {
   ifstream fin("date.in");
   ofstream fout("date.out");
-  int n, c, var;
+  int n, c, v[2][100]={0}, val;
   bool found  = false;
-  fin >> n >> c;
+  fin >> n >> c ;
   while(n!=0)
   {
-    for(int i=1; i<=c; i++)
+    for(int i=0; i<c; i++)
     {
-      fin >> var;
-      if(i<var && !found)
-      {
-        fout << i << endl;
-        found = true;
-      }
+      fin >> v[0][i];
+      cout << v[0][i] << " ";
     }
-    found =  false;
+    cout << endl;
+    for(int i=1; i<=100; i++)
+    {
+        val=0;
+        while(val<i)
+        {
+            for(int j=c-1; j>=0; j--)
+            {
+                cout << val << " -- " << v[0][j] << endl;
+                if(val+v[0][j]==i)
+                {
+                    val += v[0][j];
+                    v[1][j]++;
+                    break;
+                }
+                if(val + v[0][j]<i)
+                {
+                    val += v[0][j];
+                    v[1][j]++;
+                }
+            }
+        }
+        //cout << val << endl;
+        for(int j=0; j<c; j++)
+        {
+            if(v[1][j]!=0)
+            {
+                fout << i << ": " << v[1][j] << "x" << v[0][j] << "  ";
+            }
+            v[1][j] = 0;
+        }
+        fout << endl;
+    }
     n--;
   }
 }
